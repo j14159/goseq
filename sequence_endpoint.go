04c0fc50ab22ebd236c2a5ecaece2e0c32bb83f0
Jeremy Pierre <j.14159@gmail.com>
2013-05-13 20:30:31 -0700

    Improved short json sequence handling with rests (-1) and ties (-2).

--- sequence_endpoint.go
+++ sequence_endpoint.go
@@ -35,29 +35,51 @@ type SimpleJsonSequence struct {
 	Steps       []int
 }
 
-//Converts a list of integers to a list of note on/off 
-//events suitable for a Sequence.
-//Used to change SimpleJsonSequence step arrays
-//to real note on/off events.
+//Converts a list of integers to a list of note on/off events suitable for a Sequence.  
+//Used to change SimpleJsonSequence step arrays to real note on/off events.  -1 indicates 
+//a rest (sends a note off for the last note seen) and a -2 indicates a tie (no immediate note off).
 func intsToSteps(notes []int) []Step {
 	steps := make([]Step, len(notes))
 
+    
+
 	if len(notes) > 0 {
+        lastNote := notes[len(notes) -1]
 		for i, n := range notes {
+            var stepNotes []gocmc.NoteEvent
+            if n > -1 {
+                //if we have a note, put it in slot 1
+                stepNotes = make([]gocmc.NoteEvent, 2)
+                lastNote = n
+                stepNotes[0] = gocmc.NoteEvent{ false, lastNote, 0 }
+                stepNotes[1] = gocmc.NoteEvent{ true, n, 127 }
+            } else if n == -1 {
+                stepNotes = make([]gocmc.NoteEvent, 1)
+                stepNotes[0] = gocmc.NoteEvent{ false, lastNote, 0 }
+            } else {
+                stepNotes = make([]gocmc.NoteEvent, 0) //empty step for a tie
+            }
+
+            steps[i] = Step{ stepNotes }
+
+            /*
 			stepNotes := make([]gocmc.NoteEvent, 2)
-			stepNotes[1] = gocmc.NoteEvent{true, n, 127}
-			if i == 0 {
-				stepNotes[0] = gocmc.NoteEvent{false, notes[len(notes)-1], 0}
+            if n > -1 {
+			 stepNotes[1] = gocmc.NoteEvent{true, n, 127}
+             lastNote = n
+            }
+			if i == 0 && notes[len(notes) - 1] > -1 {
+				stepNotes[0] = gocmc.NoteEvent{ false, notes[len(notes)-1], 0 }
 			} else {
-				stepNotes[0] = gocmc.NoteEvent{false, notes[i-1], 0}
+                if n != -2 {
+				    stepNotes[0] = gocmc.NoteEvent{false, lastNote, 0}
+                }
 			}
 
-			steps[i] = Step{stepNotes}
+            if n > -1 {
+			 steps[i] = Step{stepNotes}
+            }*/
 		}
-	} else {
-		stepNotes := make([]gocmc.NoteEvent, 1)
-		stepNotes[0] = gocmc.NoteEvent{true, notes[0], 127}
-		steps[0] = Step{stepNotes}
 	}
 
 	return steps
